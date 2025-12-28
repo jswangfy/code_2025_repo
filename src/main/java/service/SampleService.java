23a9d7d3b6bbdbd3f22710c6a78a9fd5f2d8e7fb
jswangfy <ynkmwfy@gmail.com>
2025-12-28 10:22:48 -0500

    version 2.2

--- src/main/java/service/SampleService.java
+++ src/main/java/service/SampleService.java
@@ -10,4 +10,9 @@ public class SampleService {
 		System.out.println("It is service 2 in main branch");
 	}
 
+	public void service3() {
+		System.out.println("It is service 3 in main branch");
+	}
+
+	
 }
