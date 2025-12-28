f077bb12971aba4aab6bfee459d0686d468d157f
jswangfy <ynkmwfy@gmail.com>
2025-12-28 11:22:49 -0500

    version 2.75

--- src/main/java/service/SampleService.java
+++ src/main/java/service/SampleService.java
@@ -16,11 +16,7 @@ public class SampleService {
 	}
 
 	public void service4() {
-<<<<<<< HEAD
-		System.out.println("It is the service 4-3");
-=======
-		System.out.println("It is the service 4-1");
->>>>>>> 56cd6dddb4520e51116fa0f8fa035d832492f417
+		System.out.println("It is the service 4");
 	}
 	
 }
